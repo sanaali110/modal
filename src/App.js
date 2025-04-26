2c2ce5b0a22dcd1bddc9e720f82e45136c9c3b72
Abhinav Ashesh <ashesh.nitd@gmail.com>
2025-04-26 12:02:19 +0100

    impl: context based modal

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
-import { useState } from 'react';
-import './App.css';
-import Login from './view/Login';
-import Modal from './components/Modal/Modal';
-import LoginConfirmation from './view/LoginConfirmation';
+import { useState } from "react";
+import "./App.css";
+import Login from "./view/Login";
+import Modal from "./components/Modal/Modal";
+import { createContext } from "react";
 
+export const ModalContext = createContext();
 
 function App() {
   const [showModal, setShowModal] = useState(false);
-  const [currentView, setCurrentView] = useState("login")
+  const [modalContent, setModalContent] = useState();
+  const [currentView, setCurrentView] = useState("login");
 
   const handleClick = () => {
-    setShowModal(!showModal)
-  }
+    setShowModal(!showModal);
+  };
 
   const handleLoginClick = () => {
-    setCurrentView("loginConfirmation")
-  }
+    setCurrentView("loginConfirmation");
+    setShowModal(true);
+  };
 
   const getModalTitle = () => {
     if (currentView === "login") {
-      return "Login"
+      return "Login";
     } else if (currentView === "loginConfirmation") {
-      return "Login Confirmation"
+      return "Login Confirmation";
     }
-  }
+  };
 
   return (
-    <div className="App">
-      <button type='button' className='button' onClick={handleClick}>Login to the dashboard</button>
-      {showModal && <Modal title={getModalTitle()}>
-        {currentView === "login" && <Login onLogin={handleLoginClick} onCancel={() => setShowModal(false)} />}
-        {currentView === "loginConfirmation" && <LoginConfirmation onCancel={() => {
-          setCurrentView("login")
-          setShowModal(false)
-        }} />}
-      </Modal>}
-    </div>
+    <ModalContext.Provider
+      value={{ setModalContent: (content) => setModalContent(content) }}
+    >
+      <div className="App">
+        <button type="button" className="button" onClick={handleClick}>
+          Login to the dashboard
+        </button>
+        {showModal && (
+          <Modal title={getModalTitle()}>
+            {currentView === "login" ? (
+              <Login
+                onLogin={handleLoginClick}
+                onCancel={() => setShowModal(false)}
+              />
+            ) : (
+              <>{modalContent}</>
+            )}
+          </Modal>
+        )}
+      </div>
+    </ModalContext.Provider>
   );
 }
 
--- src/view/Login.jsx
+++ src/view/Login.jsx
@@ -1,11 +1,28 @@
+import { useContext } from "react"
+import { ModalContext } from "../App"
+import LoginConfirmation from "./LoginConfirmation"
+
 const Login = ({ onCancel, onLogin }) => {
+    const modalContext = useContext(ModalContext)
+
+    const onLoginClick = () => {
+        modalContext.setModalContent(
+            <LoginConfirmation
+              onCancel={() => {
+                modalContext.setModalContent(undefined);
+                onCancel()
+              }}
+            />
+          );
+        onLogin()
+    }
     // make and api call
     // fetch("/login") //post call - with request body {username, password}
     return (
         <form>
             <input type="email" className='input-styles' placeholder='type your email' />
             <input type="password" className='input-styles' placeholder='type your password' />
-            <button className='button login-button' onClick={onLogin}>Login</button>
+            <button className='button login-button' onClick={onLoginClick}>Login</button>
             <button className='button' onClick={onCancel}>Cancel</button>
         </form>
     )
